ba26aa6f4b59f91027a9ea7e248ba61c8cf01eb7
Magnus Ihse Bursie <mag@icus.se>
2022-04-16 18:01:32 +0200

    Add keybinding to open mod settings (fixes #3)

--- src/main/java/se/icus/mag/modsettings/Main.java
+++ src/main/java/se/icus/mag/modsettings/Main.java
@@ -1,8 +1,14 @@
 package se.icus.mag.modsettings;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.lwjgl.glfw.GLFW;
+import se.icus.mag.modsettings.gui.ModSettingsScreen;
 
 public class Main implements ClientModInitializer {
     public static final Logger LOGGER = LogManager.getLogger("modsettings");
@@ -10,5 +16,13 @@ public class Main implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         ModRegistry.getInstance().registerMods();
+
+        KeyBinding modSettingsKey = KeyBindingHelper.registerKeyBinding(new KeyBinding("modsettings.key.open", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_F6, "modsettings.key.category"));
+
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            while (modSettingsKey.wasPressed()) {
+                client.getInstance().setScreen(new ModSettingsScreen(null));
+            }
+        });
     }
 }
