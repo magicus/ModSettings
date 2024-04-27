7fdf7308ff066d642725366d2ff2ee18d4eaa8b1
Magnus Ihse Bursie <mag@icus.se>
2024-04-28 00:02:45 +0200

    chore: Fix incorrect whitespace

--- src/main/java/se/icus/mag/modsettings/gui/screen/ModSettingsScreen.java
+++ src/main/java/se/icus/mag/modsettings/gui/screen/ModSettingsScreen.java
@@ -13,47 +13,47 @@ import se.icus.mag.modsettings.gui.widget.Button;
 import se.icus.mag.modsettings.gui.widget.ModListWidget;
 
 public class ModSettingsScreen extends TitledScreen {
-	private static final int FULL_BUTTON_WIDTH = 200;
-	private static final int BUTTON_HEIGHT = 20;
+    private static final int FULL_BUTTON_WIDTH = 200;
+    private static final int BUTTON_HEIGHT = 20;
 
-	private ModListWidget list;
-	private boolean initIsProcessing;
+    private ModListWidget list;
+    private boolean initIsProcessing;
 
-	public ModSettingsScreen(Screen previous) {
-		super(Text.translatable("modsettings.screen.title"), previous);
-	}
+    public ModSettingsScreen(Screen previous) {
+        super(Text.translatable("modsettings.screen.title"), previous);
+    }
 
-	@Override
-	protected void init() {
-		// Protect against mods like Content Creator Integration that triggers
-		// a recursive call of Screen.init() while creating the settings screen...
-		if (initIsProcessing) return;
-		initIsProcessing = true;
+    @Override
+    protected void init() {
+        // Protect against mods like Content Creator Integration that triggers
+        // a recursive call of Screen.init() while creating the settings screen...
+        if (initIsProcessing) return;
+        initIsProcessing = true;
 
-		// Put list between 32 pixels from top and bottom
-		this.list = new ModListWidget(this.client, this.width, this.height - 64,  32, 25);
-		this.list.addAll(getAllModConfigOptions());
+        // Put list between 32 pixels from top and bottom
+        this.list = new ModListWidget(this.client, this.width, this.height - 64, 32, 25);
+        this.list.addAll(getAllModConfigOptions());
 
-		this.addSelectableChild(this.list);
-		this.addDrawableChild(this.list);
-		this.addDrawableChild(new Button(this.width / 2 - FULL_BUTTON_WIDTH / 2, this.height - 27,
-				FULL_BUTTON_WIDTH, BUTTON_HEIGHT, ScreenTexts.DONE,
-				button -> this.client.setScreen(this.previous)));
-		initIsProcessing = false;
-	}
+        this.addSelectableChild(this.list);
+        this.addDrawableChild(this.list);
+        this.addDrawableChild(new Button(this.width / 2 - FULL_BUTTON_WIDTH / 2, this.height - 27,
+                FULL_BUTTON_WIDTH, BUTTON_HEIGHT, ScreenTexts.DONE,
+                button -> this.client.setScreen(this.previous)));
+        initIsProcessing = false;
+    }
 
-	private ModConfigInfo[] getAllModConfigOptions() {
-		List<ModConfigInfo> options = new LinkedList<>();
-		for (String modId : ModRegistry.getInstance().getVisibleModIds(true, "")) {
-			try {
-				Screen configScreen = ModRegistry.getInstance().getConfigScreen(modId, this);
-				if (configScreen != null) {
-					options.add(new ModConfigInfo(modId, ModRegistry.getInstance().getModName(modId), configScreen));
-				}
-			} catch (Throwable e) {
-				Main.LOGGER.error("Error creating Settings screen from mod " + modId, e);
-			}
-		}
-		return options.toArray(new ModConfigInfo[0]);
-	}
+    private ModConfigInfo[] getAllModConfigOptions() {
+        List<ModConfigInfo> options = new LinkedList<>();
+        for (String modId : ModRegistry.getInstance().getVisibleModIds(true, "")) {
+            try {
+                Screen configScreen = ModRegistry.getInstance().getConfigScreen(modId, this);
+                if (configScreen != null) {
+                    options.add(new ModConfigInfo(modId, ModRegistry.getInstance().getModName(modId), configScreen));
+                }
+            } catch (Throwable e) {
+                Main.LOGGER.error("Error creating Settings screen from mod " + modId, e);
+            }
+        }
+        return options.toArray(new ModConfigInfo[0]);
+    }
 }
