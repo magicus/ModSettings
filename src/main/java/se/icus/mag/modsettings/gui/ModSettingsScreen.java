f0a4fabec0e9279bf3ccfa2987453eecffae5b79
Magnus Ihse Bursie <mag@icus.se>
2026-07-09 19:03:10 +0200

    refactor: Use GameOptionsScreen instead

--- src/main/java/se/icus/mag/modsettings/gui/ModSettingsScreen.java
+++ src/main/java/se/icus/mag/modsettings/gui/ModSettingsScreen.java
@@ -1,31 +1,24 @@
 package se.icus.mag.modsettings.gui;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.client.gui.widget.Positioner;
-import net.minecraft.client.gui.widget.TextWidget;
-import net.minecraft.client.gui.widget.ThreePartsLayoutWidget;
-import net.minecraft.screen.ScreenTexts;
+import net.minecraft.client.gui.screen.option.GameOptionsScreen;
+import net.minecraft.client.gui.widget.ClickableWidget;
 import net.minecraft.text.Text;
 import se.icus.mag.modsettings.Main;
 import se.icus.mag.modsettings.ModRegistry;
 
-import java.util.LinkedList;
-import java.util.List;
-
-public class ModSettingsScreen extends Screen {
-	private static final int FULL_BUTTON_WIDTH = 200;
+public class ModSettingsScreen extends GameOptionsScreen {
+	private static final int BUTTON_WIDTH = 150;
 	private static final int BUTTON_HEIGHT = 20;
-	private static final int LIST_TOP_MARGIN = 32;
-	private static final int LIST_BOTTOM_MARGIN = 32;
-
-	private final Screen previous;
-	private ThreePartsLayoutWidget layout;
-	private ModListWidget list;
 	private boolean initIsProcessing;
 
 	public ModSettingsScreen(Screen previous) {
-		super(Text.translatable("modsettings.screen.title"));
-		this.previous = previous;
+		super(previous, MinecraftClient.getInstance().options, Text.translatable("modsettings.screen.title"));
 	}
 
 	@Override
@@ -34,26 +27,21 @@ public class ModSettingsScreen extends Screen {
 		// a recursive call of Screen.init() while creating the settings screen...
 		if (initIsProcessing) return;
 		initIsProcessing = true;
-
-		this.layout = new ThreePartsLayoutWidget(this, LIST_TOP_MARGIN, LIST_BOTTOM_MARGIN);
-		this.layout.addHeader(new TextWidget(this.title, this.textRenderer), Positioner::alignHorizontalCenter);
-
-		this.list = new ModListWidget(this.client, this.width, this.height - LIST_TOP_MARGIN - LIST_BOTTOM_MARGIN, LIST_TOP_MARGIN, 25);
-		this.list.addAll(getAllModConfigOptions());
-		this.layout.addBody(this.list);
-
-		this.layout.addFooter(new Button(0, 0, FULL_BUTTON_WIDTH, BUTTON_HEIGHT, ScreenTexts.DONE,
-				button -> this.client.setScreen(this.previous)), Positioner::alignHorizontalCenter);
-		this.layout.forEachChild(child -> this.addDrawableChild(child));
-		this.refreshWidgetPositions();
-		initIsProcessing = false;
+		try {
+			super.init();
+		} finally {
+			initIsProcessing = false;
+		}
 	}
 
 	@Override
-	protected void refreshWidgetPositions() {
-		if (this.layout != null) {
-			this.layout.refreshPositions();
-		}
+	protected void addOptions() {
+		ModSettingsOption[] options = getAllModConfigOptions();
+		List<ClickableWidget> buttons = Arrays.stream(options)
+				.map(option -> new Button(0, 0, BUTTON_WIDTH, BUTTON_HEIGHT, Text.of(option.modName()),
+						button -> this.client.setScreen(option.configScreen())))
+				.collect(Collectors.toList());
+		this.body.addAll(buttons);
 	}
 
 	private ModSettingsOption[] getAllModConfigOptions() {
@@ -71,11 +59,6 @@ public class ModSettingsScreen extends Screen {
 		return options.toArray(new ModSettingsOption[0]);
 	}
 
-	@Override
-	public void close() {
-		this.client.setScreen(this.previous);
-	}
-
-	public record ModSettingsOption(String modId, String modName, Screen configScreen)  {
+	public record ModSettingsOption(String modId, String modName, Screen configScreen) {
 	}
 }
